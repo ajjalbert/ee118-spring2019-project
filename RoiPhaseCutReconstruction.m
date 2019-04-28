51dc28c1f5888d2cc9c1701d44383589232a2388
Andrew Jalbert <aj.jalbert@outlook.com>
2019-04-27 21:10:54 -0700

    Updated Amplitude
    Amplitude works now, but phase map is still awful. Need to fix phase map.

--- RoiPhaseCutReconstruction.m
+++ RoiPhaseCutReconstruction.m
@@ -1,9 +1,9 @@
 
 % Load in the image
-cell=imread('imwf.tif');
+picture=imread('imwf.tif');
 
 % Do a 2D FFT of the image
-Y = fft2(cell);
+Y = fft2(picture);
 % Display the FFT. Need to shift the fft, get real values, and then log it
 % to exaggerate our picture.
 figure(1);
@@ -17,15 +17,14 @@ yMask = createMask(frtCirc);
 
 % Create a modified version of the shifted fourier image
 % This will take only the image places we want to analyze
-YPrime = fftshift(fft2(cell)).*yMask;
-
+YPrime = yMask.*(fftshift(Y));
 
 % Shift the new image to the center
 % Get center of the circle
 frtCenter = round(frtCirc.Center);
 
 % Get size of our image
-[a,b,c] = size(cell);
+[a,b,c] = size(picture);
 
 % Find the distance to shift the masked image
 dM = abs(frtCenter(1,1) - a/2);
@@ -34,17 +33,26 @@ dN = abs(frtCenter(1,2) - b/2);
 % Shift the masked image!
 YPShifted = circshift(YPrime, [dM dN]);
 
-figure(69);
-imshow(YPShifted);
+% Image to see where we shifted to!
+figure(394);
+imagesc(log(abs(YPShifted)));
+colormap(gray);
+colorbar;
 
 % Get this image back to real space!
-YFilt = ifft2(ifftshift(YPrime));
+YFilt = ifft2(YPShifted);
+
+% Image to see if we have image again, but filtered
+figure(71);
+imagesc((log(abs(YFilt))));
+colormap(gray);
+colorbar;
 
 % Load the background image
-cellBckgd=imread('imbkgd.tif');
+bckgd=imread('imbkgd.tif');
 
 % Find the 2D fourier transform
-Z = fft2(cellBckgd);
+Z = fft2(bckgd);
 
 % Display the FFT. Need to shift the fft, get real values, and then log it
 % to exaggerate our picture.
@@ -57,14 +65,14 @@ zMask = createMask(bckgdCirc());
 
 % Use the cropped mask to modify our fourier image
 % This allows us to work with the data we want
-ZPrime = fftshift(Z).*zMask;
+ZPrime = zMask.*(fftshift(Z));
 
 % Shift the modified image to the center
 % Get center of the circle
 bckgdCenter = round(bckgdCirc.Center);
 
 % Get size of our image
-[aPrime,bPrime,cPrime] = size(cellBckgd);
+[aPrime,bPrime,cPrime] = size(bckgd);
 
 % Find the distance to shift the masked image
 dMPrime = -(bckgdCenter(1,1) - aPrime/2);
@@ -73,27 +81,40 @@ dNPrime = -(bckgdCenter(1,2) - bPrime/2);
 % Now shift the image by the required distance
 ZPShifted = circshift(ZPrime,[dMPrime dNPrime]);
 
-figure(70);
-imshow(ZPShifted);
+% Image to see where we shifted to!
+figure(419);
+imagesc(log(abs(ZPShifted)));
+colormap(gray);
+colorbar;
+
 
 % Get this image back to real space!!
-ZFilt = ifft2(ifftshift(ZPrime));
+ZFilt = ifft2(ZPShifted);
+
+% Image to see if we have background again, but filtered
+figure(72);
+imagesc(log(abs(ZFilt)));
+colormap(gray);
+colorbar;
 
 % Now take the difference of our filtered images
-% This will give us the Quantitative-phase
-cellFilt = angle(YFilt) - angle(ZFilt);
+% This will give us the Quantitative-phase and amplitude
+phase = angle(YFilt) - angle(ZFilt);
 
-cellAmp = abs(YFilt) - abs(ZFilt);
-
-% Now unwrap the unwanted phase lines!
+% Image amplitude
+amp = abs(YFilt) - abs(ZFilt);
+pi = 3.14;
+% Now unwrap the unwanted phase lines from angle!
 % Hopefully the image is pretty now
-Q = unwrap(cellFilt,-4);
-P = unwrap(cellAmp,-4);
+Q = unwrap(phase,-pi);
 
 % Congrats! We have done a fourier thing!
 figure(3);
-imshow(imadjust(Q));
+imagesc(log(abs(phase)));
+colormap(gray);
+colorbar;
 
 figure(4);
-
-imshow(imadjust(cellAmp));
+imagesc(amp);
+colormap(gray);
+colorbar;
